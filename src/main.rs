169795e7f6d5721091b54a2cd2faae8c0eec1c54
Alexandre Pasmantier <alex.pasmant@gmail.com>
2023-11-26 18:49:32 +0100

    pipe stdout and stderr

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,19 @@
-use anyhow::{Context, Result};
+use std::io::{self, Write};
+
+use anyhow::Result;
 
 // Usage: your_docker.sh run <image> <command> <arg1> <arg2> ...
 fn main() -> Result<()> {
-    // Uncomment this block to pass the first stage!
     let args: Vec<_> = std::env::args().collect();
     let command = &args[3];
     let command_args = &args[4..];
     let output = std::process::Command::new(command)
         .args(command_args)
         .output()
-        .with_context(|| {
-            format!(
-                "Tried to run '{}' with arguments {:?}",
-                command, command_args
-            )
-        })?;
+        .expect("failed to execute subprocess");
 
-    if output.status.success() {
-        let std_out = std::str::from_utf8(&output.stdout)?;
-        println!("{}", std_out);
-    } else {
-        std::process::exit(1);
-    }
+    io::stdout().write_all(&output.stdout).unwrap();
+    io::stderr().write_all(&output.stderr).unwrap();
 
     Ok(())
 }
