f10e7ba6bbf8f3779ba5a498b462d67d16633392
Alexandre Pasmantier <alex.pasmant@gmail.com>
2023-12-30 12:31:45 +0100

    refactoring

--- src/main.rs
+++ src/main.rs
@@ -4,15 +4,45 @@ use anyhow::Result;
 
 // Usage: your_docker.sh run <image> <command> <arg1> <arg2> ...
 fn main() -> Result<()> {
-    let args: Vec<_> = std::env::args().collect();
-    let command = &args[3];
-    let command_args = &args[4..];
-    Command::new(command)
-        .args(command_args)
+    let args = std::env::args().collect();
+    let arguments = parse_args(args);
+    // println!("Image: {}", &arguments.image);
+    // println!(
+    //     "Command: {} {}",
+    //     &arguments.command,
+    //     &arguments.command_arguments.join(" ")
+    // );
+
+    Command::new(arguments.command)
+        .args(arguments.command_arguments)
         .stdout(Stdio::inherit())
         .stderr(Stdio::inherit())
         .output()
         .expect("Failed to execute command");
 
     Ok(())
+}
+
+#[derive(Debug)]
+struct Arguments {
+    image: String,
+    command: String,
+    command_arguments: Vec<String>,
+}
+
+fn parse_args(args: Vec<String>) -> Arguments {
+    let mut arguments = args.into_iter();
+    let executable = arguments.next().unwrap();
+    let docker_command = arguments
+        .next()
+        .expect("please provide a docker command to use");
+    let image = arguments.next().expect("please provide an image to run");
+    let command = arguments
+        .next()
+        .expect("please provide a command to run on your image");
+    Arguments {
+        image,
+        command,
+        command_arguments: arguments.collect(),
+    }
 }
